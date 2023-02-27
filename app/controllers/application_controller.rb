d9bd7e0ea6170e08361dbb1ea66e0caf2f1977cd
Koushal-Modi <koushal@preciousinfosystem.com>
2023-02-27 19:57:51 +0530

    Added Styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:email,:first_name, :last_name, :user_name,  :password, :password_configuration])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:email,:first_name, :last_name, :user_name, :password, :password_configuration])
+    devise_parameter_sanitizer.permit(:account_update, keys:[:email,:first_name, :last_name, :user_name, :password, :password_configuration, :current_password])
   end
 
 end
